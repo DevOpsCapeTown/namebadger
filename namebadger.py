e772c797779fb8eb24c69316b708395f5820cb97
Tom Bamford <tom@atplanet.co.za>
2018-09-04 10:28:31 +0100

    Amend for DODCPT2018 event

--- namebadger.py
+++ namebadger.py
@@ -1,65 +1,89 @@
 #!/usr/bin/env python3
 
 from copy import copy
-import csv
-import io
-import os
-import re
+import csv, io, os, re
 from PyPDF2 import PdfFileReader, PdfFileWriter
-from reportlab.lib.pagesizes import A3
+from reportlab.lib.pagesizes import A3, A4
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 
-pdf_source = open('badges-a3-double.pdf', 'rb')
-pdf_in = PdfFileReader(pdf_source)
+CSV_FILE = 'devopsdays-cape-town-2018_guestlist.csv'
+FIRSTNAME_COLUMN = 30
+LASTNAME_COLUMN = 31
 
+PDF_TEMPLATE = 'A3-Layout-Attendee-Test.pdf'
+PDF_SIZE = A3
+#PDF_SIZE = A4
+
+
+a3_positions_2017 = [
+    [[106, 1026], [406, 1026]],
+    [[106, 725], [406, 725]],
+    [[106, 422], [406, 422]],
+]
+
+a3_positions_2018 = [
+    [[112, 1026], [430, 1026]],
+    [[112, 714], [430, 714]],
+    [[112, 400], [430, 400]],
+]
+
+a4_positions = [
+    [[130, 410], [430, 410]],
+]
+
+POSITIONS = a3_positions_2018
+
+#TEXT_RGB = (6, 60, 91) # blue
+TEXT_RGB = (16, 95, 89) # green
+
+TEXT_START_SIZE = 24
+
+TEXT_SIZE_THRESHOLDS = (
+    (220, 16),
+    (210, 17),
+    (195, 18),
+    (180, 19),
+    (170, 20),
+    (160, 21),
+    (150, 22),
+    (140, 23),
+)
+
+FIRSTNAME_FONT = 'Avenir-Bold'
+LASTNAME_FONT = 'Avenir'
+
+pdf_source = open(PDF_TEMPLATE, 'rb')
+pdf_in = PdfFileReader(pdf_source)
 
 def determine_size(thing, base_size):
     thing_len = pdfmetrics.stringWidth(thing, 'Avenir', 24)
     print("{} = {}".format(thing, thing_len))
-    thresholds = (
-        (220, 16),
-        (210, 17),
-        (195, 18),
-        (180, 19),
-        (170, 20),
-        (160, 21),
-        (150, 22),
-        (140, 23),
-    )
-    for threshold in thresholds:
+    for threshold in TEXT_SIZE_THRESHOLDS:
         if thing_len > threshold[0] and base_size > threshold[1]:
             return threshold[1]
     return base_size
 
 
-def names_page(names):
+def names_page(names, positions):
     packet = io.BytesIO()
-    can = canvas.Canvas(packet, pagesize=A3)
+    can = canvas.Canvas(packet, pagesize=PDF_SIZE)
 
     fonts_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fonts')
     pdfmetrics.registerFont(TTFont("Avenir", os.path.join(fonts_dir, 'avenir.ttf')))
     pdfmetrics.registerFont(TTFont("Avenir-Bold", os.path.join(fonts_dir, 'avenir_bold.ttf')))
 
-    can.setFillColorRGB(6/256, 60/256, 91/256)
-
-    positions = [
-        [[110, 1056], [408, 1056]],
-        [[110, 740], [408, 740]],
-        [[110, 424], [408, 424]],
-    ]
+    can.setFillColorRGB(TEXT_RGB[0]/256, TEXT_RGB[1]/256, TEXT_RGB[2]/256)
 
     for index, name in enumerate(names):
         for c in range(0,2):
             x = positions[index][c][0]
             y = positions[index][c][1]
-            can.setFont('Avenir-Bold', determine_size(name[0], 24))
+            can.setFont(FIRSTNAME_FONT, determine_size(name[0], TEXT_START_SIZE))
             can.drawString(x, y, name[0].upper())
-            can.setFont('Avenir', determine_size(name[1], 24))
+            can.setFont(LASTNAME_FONT, determine_size(name[1], TEXT_START_SIZE))
             can.drawString(x, y-26, name[1].upper())
-            #can.setFont('Avenir', determine_size(name[2], 20))
-            #can.drawString(x, y-26-32, name[2])
 
     can.showPage()
     can.save()
@@ -73,8 +97,9 @@ def load_csv(path):
         reader = csv.reader(f)
         next(reader)
         for row in reader:
-            last_name = row[26].strip()
-            first_name = re.sub('{}$'.format(re.escape(last_name)), '', row[25].strip(), flags=re.IGNORECASE)
+            last_name = row[LASTNAME_COLUMN].strip()
+            #first_name = re.sub('{}$'.format(re.escape(last_name)), '', row[25].strip(), flags=re.IGNORECASE)
+            first_name = row[FIRSTNAME_COLUMN].strip()
             attendees.append([first_name, last_name])
     return attendees
 
@@ -82,14 +107,17 @@ def load_csv(path):
 output = open('out.pdf', 'wb')
 writer = PdfFileWriter()
 
-attendees = load_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'devopsdays-cape-town-2016_guestlist.csv'))
+attendees = load_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)), CSV_FILE))
+#attendees = attendees[0:12]
+
 #import pprint
 #pprint.PrettyPrinter().pprint(attendees)
 #import sys
 #sys.exit(0)
-chunks = [attendees[x:x+3] for x in range(0, len(attendees), 3)]
+
+chunks = [attendees[x:x+len(POSITIONS)] for x in range(0, len(attendees), len(POSITIONS))]
 for chunk in chunks:
-    packet = names_page(chunk)
+    packet = names_page(chunk, POSITIONS)
     pdf_new = PdfFileReader(packet)
     page = copy(pdf_in.getPage(0))
     page.mergePage(pdf_new.getPage(0))
